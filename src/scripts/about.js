2dd811a472db9b41ddd674c49c1be5d58545f513
Maria Kotliarova <itwassoclear@gmail.com>
2020-06-19 15:26:18 +0300

    index and about are ready

--- src/scripts/about.js
+++ src/scripts/about.js
@@ -1,3 +1,17 @@
 import "../pages/about.css";
 
 console.log('About')
+
+var Flickity = require('flickity');
+
+var flkty = new Flickity( '.main-carousel', {
+  // options
+  draggable: true, // можно свайпать
+  // freeScroll: true, // свободное пролистывание
+  cellAlign: 'center', // выбранный слайд располагается в центре
+  // wrapAround: true, // перемотка по кругу
+  contain: true, // ограничивает перемотку количеством коммитов
+  initialIndex: 2, // выбор начального (центрального) слайда
+});
+
+
