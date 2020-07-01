e119d156676e9f36e0d738aa80cc42e98a04baf0
Maria Kotliarova <itwassoclear@gmail.com>
2020-07-01 17:52:38 +0300

    commit history is ready, work with github api

--- src/index.js
+++ src/index.js
@@ -1,3 +1,159 @@
 import "./styles/index.css";
 
-console.log('Index')
+const searchButton = document.querySelector('.search__button'); // кнопка Искать
+const searchingResults = document.querySelector('.searching-results'); // общий контейнер для всех результатов поиска
+const preloader = document.querySelector('.preloader'); // блок Прелоадер
+const notFound = document.querySelector('.not-found'); // блок Ничего не найдено
+const results = document.querySelector('.results'); // блок Результаты поиска
+const cards = document.querySelector('.cards'); // контейнер для карточек
+const showMore = document.querySelector('.results__button'); // кнопка Показать ещё
+
+const monthArray = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
+
+// базовый компонент
+class BaseComponent {
+  constructor(handlers = []) {
+    this.handlers = handlers;
+  }
+
+  _setHandlers() {
+    this.handlers.forEach(handler => this.addHandler(handler));
+  }
+  addHandler({ elem, event, callback }) {
+    elem.addEventListener(event, callback);
+  }
+  clearHandlers() {
+    this.handlers.forEach(handler => this.removeHandler(handler));
+  }
+  removeHandler({ elem, event, callback }) {
+    elem.removeEventListener(event, callback);
+  }
+}
+
+// наследование базового компонента для формы
+class Form extends BaseComponent {
+  constructor(handlers) {
+    super(handlers);
+  }
+}
+
+const inputElem = document.querySelector('.search__input'); // инпут
+const formElem = document.querySelector('.search__form'); // форма с поиском
+const inputErrors = document.querySelector('.search__error-message'); // ошибка валидации
+
+const onSubmitCallback = (event) => {
+  event.preventDefault();
+  console.log('Submit!!!')
+}
+
+const onInputCallback = (event) => {
+  const inputElem = event.target;
+  if (inputElem.validity.valueMissing) {
+    inputElem.setCustomValidity('Нужно ввести ключевое слово');
+    searchButton.setAttribute('disabled', true);
+  } else if (inputElem.validity.tooShort) {
+    inputElem.setCustomValidity('Должно быть от 2 до 30 символов');
+    searchButton.setAttribute('disabled', true);
+  } else {
+    inputElem.setCustomValidity('');
+    searchButton.removeAttribute('disabled');
+  }
+  inputErrors.textContent = inputElem.validationMessage;
+}
+
+const form = new Form([
+  {
+    elem: inputElem,
+    event: 'input',
+    callback: onInputCallback,
+  },
+  {
+    elem: formElem,
+    event: 'submit',
+    callback: onSubmitCallback,
+  },
+]);
+
+form._setHandlers();
+// form.clearHandlers();
+
+// создание карточки
+class NewsCard {
+  createCard(link, image, date, title, text, label) {
+    const card = document.createElement('div'); // карточка
+    card.classList.add('card');
+
+    const cardLink = document.createElement('a'); // контейнер, ссылка
+    cardLink.classList.add('card__link');
+    cardLink.setAttribute('href',`${link}`);
+    cardLink.setAttribute('target','_blank');
+
+    const cardImage = document.createElement('img'); // картинка
+    cardImage.classList.add('card__image');
+    cardImage.classList.add('style', `background-image: url('${image}')`)
+    cardImage.setAttribute('alt', `Изображение из статьи`);
+
+    const cardDate = calculateDate(date); // дата
+
+    const cardDescription = document.createElement('div'); // текстовый блок
+    cardDescription.classList.add('card__description');
+
+    const cardTitle = document.createElement('h3'); // заголовок
+    cardTitle.classList.add('card__title');
+    cardTitle.textContent = title;
+
+    const cardText = document.createElement('p'); // текст
+    cardText.classList.add('card__text');
+    cardText.textContent = text;
+
+    const cardLabel = document.createElement('p'); // источник
+    cardLabel.classList.add('card__label');
+    cardLabel.textContent = label;
+
+    card.appendChild(cardLink);
+    cardLink.appendChild(cardImage);
+    cardLink.appendChild(cardDate);
+    cardLink.appendChild(cardDescription);
+    cardDescription.appendChild(cardTitle);
+    cardDescription.appendChild(cardText);
+    cardDescription.appendChild(cardLabel);
+
+    return card;
+  }
+}
+
+class NewsCardList {
+  constructor(list){
+    this.list = list;
+    this.cardsInPage = 3;
+    this.lastCard = 0;
+  }
+}
+
+// функция вычисления даты
+function calculateDate(date) {
+  const cardDate = document.createElement('time');
+  // cardDate.classList.add('card__date');
+  cardDate.setAttribute('datetime', date);
+  let dateInfo = new Date(date);
+  cardDate.textContent = dateInfo.getDate() + ' ' + monthArray[dateInfo.getMonth()] + ', ' + dateInfo.getFullYear();
+ 
+  return cardDate;
+}
+
+const createCard = new NewsCard();
+console.log(createCard)
+
+// ньюс апи
+var url = 'http://newsapi.org/v2/everything?' +
+          'q=Apple&' +
+          'from=2020-06-27&' +
+          'sortBy=popularity&' +
+          'apiKey=772313d1aa0e42c39e8d202c20282aea';
+
+var req = new Request(url);
+
+fetch(req)
+  .then(function(response) {
+      console.log(response.json());
+  });
--- src/scripts/about.js
+++ src/scripts/about.js
@@ -1,20 +1,21 @@
 import "../styles/about.css";
 
-console.log('About')
+const github = 'https://api.github.com/repos/itwassoclear/NewsAnalyzer/commits';
+const monthArray = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'];
 
+// слайдер
 var Flickity = require('flickity');
-
-var flkty = new Flickity( '.main-carousel', {
+var flkty = new Flickity('.main-carousel', {
   // options
   draggable: true, // можно свайпать
-  // freeScroll: true, // свободное пролистывание
+  freeScroll: true, // свободное пролистывание
   cellAlign: 'center', // выбранный слайд располагается в центре
   // wrapAround: true, // перемотка по кругу
   contain: true, // ограничивает перемотку количеством коммитов
-  initialIndex: window.innerWidth >= 800 ? 3 : 0, // выбор начального (центрального) слайда
+  initialIndex: window.innerWidth >= 800 ? 3 : 0, // выбор начального слайда
 });
 
-let slider = document.querySelector('.flickity-slider');
+let slider = document.querySelector('.flickity-viewport');
 
 if (window.innerWidth <= 540) {
   slider.style = `transform: translateX(16px);`
@@ -23,3 +24,99 @@ if (window.innerWidth <= 540) {
 } else {
   slider.style = `transform: translateX(0);`
 }
+
+// апи GitHub
+class GithubApi {
+  constructor(url) {
+    this.url = url;
+  }
+
+  getCommits() {
+    return fetch(this.url, {
+      method: 'GET',
+    })
+    .then(res => {
+    if(res.ok) {
+      return Promise.resolve(res.json());
+    }
+      return Promise.reject(`Ошибка: ${res.status}`);
+    })
+}
+}
+
+const githubApi = new GithubApi(github);
+
+class CommitCard {
+  createCard(data) {
+    const carouselCard = document.createElement('div'); // карточка коммита
+    carouselCard.classList.add('carousel-card');
+
+    const date = calculateDate(data.commit.committer.date); // дата
+    date.classList.add('carousel-card__date');
+
+    const block = document.createElement('div'); // блок коммита
+    block.classList.add('carousel-card__block');
+
+    const photo = document.createElement('img'); // фото автора коммита
+    photo.classList.add('carousel-card__photo');
+    photo.setAttribute('alt', 'Фото автора коммита');
+    photo.src = `${data.author.avatar_url}`;
+
+    const about = document.createElement('div'); // блок с именем и почтой
+    about.classList.add('carousel-card__about');
+
+    const name = document.createElement('h3'); // имя
+    name.classList.add('carousel-card__name');
+    name.textContent = data.commit.committer.name;
+
+    const mail = document.createElement('p'); // почта
+    mail.classList.add('carousel-card__mail');
+    mail.textContent = data.commit.committer.email;
+
+    const commit = document.createElement('p'); // коммит
+    commit.classList.add('carousel-card__commit');
+    commit.textContent = data.commit.message;
+
+    carouselCard.appendChild(date);
+    carouselCard.appendChild(block);
+    carouselCard.appendChild(commit);
+    block.appendChild(photo);
+    block.appendChild(about);
+    about.appendChild(name);
+    about.appendChild(mail);
+
+    return carouselCard;
+  }
+}
+
+const commitCard = new CommitCard();
+
+class CommitCardList {
+  constructor(cardList) {
+    this.cardList = cardList;
+  }
+  renderCards(carouselCard) {
+    this.cardList.append(carouselCard);
+  }
+}
+
+const commitCardList = new CommitCardList(flkty);
+
+githubApi.getCommits()
+.then((res) => {
+  res.forEach(data => commitCardList.renderCards(commitCard.createCard(data)))
+})
+.catch((err) => {
+  console.log(`Ошибка: ${err}`);
+})
+
+// функция вычисления даты
+function calculateDate(date) {
+  const cardDate = document.createElement('time');
+  cardDate.setAttribute('datetime', date);
+  let dateInfo = new Date(date);
+  cardDate.textContent = dateInfo.getDate() + ' ' + monthArray[dateInfo.getMonth()] + ', ' + dateInfo.getFullYear();
+ 
+  return cardDate;
+}
+
